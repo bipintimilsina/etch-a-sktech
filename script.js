f73b8204b4195ffae1b12efdfd226263ae486351
Bipin Timilsina <timilsinabipin20@gmail.com>
2024-06-21 15:44:27 +0545

    added eraser, colorPicker, refactored and designed

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let click = false;
+let eraserMode = false;
+
+
+let divs = document.querySelectorAll(".board div");
 document.addEventListener("DOMContentLoaded", function () {
   createBoard(16, "red");
   let colorChoice = "yellow";
   let color = "yellow";
 
+  const colorPicker = document.getElementById("colorPicker");
+  colorPicker.oninput = (e) => setColor(e.target.value);
   let body = document.querySelector("body").addEventListener("click", (e) => {
     if ((e.target.tagName = "button")) {
       click = !click;
@@ -13,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (click) {
       playMsg.innerHTML = "You can draw now";
+      playMsg.style.backgroundColor='powderblue'
     } else {
       playMsg.innerHTML = "Click to draw";
     }
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let size = getSize();
     createBoard(size, colorChoice);
   });
+
+  let eraserBtn = document.querySelector("#eraser");
+  eraserBtn.addEventListener("click", eraser);
 });
 
 function setColor(colorChoice) {
@@ -31,14 +41,23 @@ function setColor(colorChoice) {
 
 function colorDiv() {
   // let color;
+  eraserMode=false;
+  if (click ) {
+eraserMode=false;
+    divs.forEach((div) => {
+      div.removeEventListener("mouseover", function () {
+        this.style.backgroundColor = "white";
+      });
+      div.addEventListener("mouseover", colorDiv);
+    });
 
-  if (click) {
-    if (color === "random") {
+
+    if (color === "random") {eraserMode=false;
       this.style.backgroundColor = `hsl(${Math.random() * 360},100%,50%)`;
     } else if (color === "black") {
       this.style.backgroundColor = "black";
     } else {
-      this.style.backgroundColor = "yellow";
+      this.style.backgroundColor = color;
     }
   }
 }
@@ -60,16 +79,57 @@ function createBoard(size, color) {
 }
 
 function reset() {
-  let divs = document.querySelectorAll("div");
+  let divs = document.querySelectorAll(".board div");
   divs.forEach((element) => (element.style.backgroundColor = "white"));
 }
 
+// function eraser() {
+//   let divs = document.querySelectorAll(".board div");
+
+//   divs.forEach((div) => {
+//     div.removeEventListener("mouseover", colorDiv);
+//     div.addEventListener("mouseover", function () {
+//       this.style.backgroundColor = "white";
+//     });
+//   });
+// }
+
 function eraser() {
-  let div = document.querySelector("div");
-  div.addEventListener("mouseover", () => {
-    this.style.backgroundColor = "white";
-  });
+  eraserMode = !eraserMode; // Toggle eraserMode when the eraser button is clicked
+
+  let divs = document.querySelectorAll(".board div");
+
+  if (eraserMode) {
+    divs.forEach((div) => {
+      // Remove the colorDiv event listener and add eraser functionality
+      div.removeEventListener("mouseover", colorDiv);
+      div.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "white";
+      });
+    });
+  } else {
+    // If eraserMode is false, reset the divs to default behavior (colorDiv)
+    divs.forEach((div) => {
+      div.removeEventListener("mouseover", function () {
+        this.style.backgroundColor = "white";
+      });
+      div.addEventListener("mouseover", colorDiv);
+    });
+  }
 }
+
+// function eraser() {
+//   // Select all div elements inside the board
+//   let divs = document.querySelectorAll(".board div");
+//   divs.forEach(div => {
+//     // Remove any existing event listeners for colorDiv
+//     div.removeEventListener("mouseover", colorDiv);
+//     // Add new event listener to set background color to white
+//     div.addEventListener("mouseover", function() {
+//       this.style.backgroundColor = "white";
+//     });
+//   });
+// }
 
 function getSize() {
   let input = prompt("What wiil be the size of board?");
